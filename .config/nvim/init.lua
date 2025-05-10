c7fb3ff44f1d7ad2ae6558812f5f4c2ff195b58e
PabloTheDeveloper <30012721+PabloTheDeveloper@users.noreply.github.com>
2025-05-09 23:12:27 -0400

    another batch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,3 +29,41 @@ map({ "i", "s" }, "<s-tab>", function()
 end, { expr = true })
 map("x", "<Tab>", "<plug>(snippy-cut-text)")
 map("n", "g<Tab>", "<plug>(snippy-cut-text)")
+
+-- Create an autocommand group specifically for markdown settings
+-- This helps keep related autocommands organized and allows clearing them easily
+vim.api.nvim_create_augroup("MarkdownSpecificSettings", { clear = true })
+
+-- Apply settings specifically for markdown files
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown", -- This pattern matches files with the 'markdown' filetype
+	group = "MarkdownSpecificSettings", -- Assign this autocommand to our group
+	callback = function()
+		-- Disable swap file for the current buffer (the markdown file)
+		-- Using vim.opt_local ensures this setting only applies to the buffer
+		vim.opt_local.swapfile = false
+
+		-- Set up an autocommand to autosave when leaving insert mode
+		-- This triggers a write command for the current buffer (buffer = 0)
+		vim.api.nvim_create_autocmd("InsertLeave", {
+			buffer = 0, -- Apply only to the current buffer where FileType markdown was detected
+			group = "MarkdownSpecificSettings", -- Use the same group
+			callback = function()
+				vim.cmd("write") -- Execute the ':write' command to save the buffer
+			end,
+		})
+
+		-- Optional: You could also add an autocommand to save after a short pause
+		-- in normal mode using 'CursorHold', but 'InsertLeave' is often sufficient
+		-- for a simple autosave while editing.
+		--[[
+    vim.api.nvim_create_autocmd('CursorHold', {
+      buffer = 0,
+      group = 'MarkdownSpecificSettings',
+      callback = function()
+        vim.cmd('write')
+      end
+    })
+    ]]
+	end,
+})
